71155023fdcd7a2b1ff08dc9a2d28452302676ca
Rajat_Dwivedi <rajat24dw@gmail.com>
2024-02-23 17:29:12 +0530

    adding cron

--- server/index.js
+++ server/index.js
@@ -5,19 +5,20 @@ import Router from './Router/route.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import Count from './models/count.js';
-
-
+import cron from "node-cron";
 
 dotenv.config();
-const app=express();
+const app = express();
 app.use(cors());
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/',Router);
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Router);
 
+const PORT = 8000;
 
-
-const PORT=8000;
-
-app.listen(PORT,()=> console.log (`Server is running successfully on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running successfully on Port ${PORT}`));
 connection();
+
+cron.schedule('*/1 * * * *', () => {
+    console.log("running every minute");
+});
